333e0930309e0475a75f0b29ccea84ab8e447125
Déborah Loisel <chinfalosteam@gmail.com>
2025-06-17 00:02:20 +0100

    Add files via upload

---
+++ js/tsparticlesInit.js
@@ -0,0 +1,13 @@
+
+tsParticles.load("tsparticles", {
+    fullScreen: { enable: false },
+    background: { color: { value: "transparent" } },
+    particles: {
+        number: { value: 60 },
+        color: { value: "#00ffd5" },
+        shape: { type: "circle" },
+        opacity: { value: 0.6 },
+        size: { value: 3 },
+        move: { enable: true, speed: 1 }
+    }
+});
