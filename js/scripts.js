2b9bcdbbe4965ffd3cc1cb7b2be19d48780480ff
Déborah Loisel <chinfalosteam@gmail.com>
2025-07-06 18:28:48 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,29 @@
+// Referencias a los elementos que abren el modal
 const openCvBtn = document.getElementById('open-cv');
+const openCvLink = document.getElementById('open-cv-link');
 const closeCvBtn = document.getElementById('close-cv');
 const cvModal = document.getElementById('cv-modal');
 
-openCvBtn.addEventListener('click', function(event) {
+function openModal(event) {
   event.preventDefault();
-  cvModal.style.display = 'flex';  // ¡Aquí debe ser flex para que funcione el modal!
-});
+  cvModal.style.display = 'flex'; // display flex para centrar contenido modal
+}
 
-closeCvBtn.addEventListener('click', function() {
+function closeModal() {
   cvModal.style.display = 'none';
-});
+}
 
+// Asignar eventos click a los triggers de abrir modal
+openCvBtn.addEventListener('click', openModal);
+openCvLink.addEventListener('click', openModal);
+
+// Evento click para cerrar modal
+closeCvBtn.addEventListener('click', closeModal);
+
+// Cerrar modal al hacer click fuera del contenido
 window.addEventListener('click', function(event) {
   if(event.target === cvModal) {
-    cvModal.style.display = 'none';
+    closeModal();
   }
 });
 
