04c48d77c76d23248e55cbda1b3223a94e14b7c0
Déborah Loisel <chinfalosteam@gmail.com>
2025-07-06 18:24:53 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ const cvModal = document.getElementById('cv-modal');
 
 openCvBtn.addEventListener('click', function(event) {
   event.preventDefault();
-  cvModal.style.display = 'flex';
+  cvModal.style.display = 'flex';  // ¡Aquí debe ser flex para que funcione el modal!
 });
 
 closeCvBtn.addEventListener('click', function() {
@@ -12,7 +12,7 @@ closeCvBtn.addEventListener('click', function() {
 });
 
 window.addEventListener('click', function(event) {
-  if (event.target === cvModal) {
+  if(event.target === cvModal) {
     cvModal.style.display = 'none';
   }
 });
